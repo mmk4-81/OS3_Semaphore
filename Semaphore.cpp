6ca50d125915ae9aaa22bfc635de0c3f546ac755
mmk <mmkkarbalaei@gmail.com>
2024-02-08 22:17:50 +0330

    create base structure of project

---
+++ Semaphore.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+using namespace std::chrono;
+
+
+int main(){
+    return 0;
+}
