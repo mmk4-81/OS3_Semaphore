b438d5051c2c471f10be4f60102659874da7540f
mmk <mmkkarbalaei@gmail.com>
2024-02-08 22:38:35 +0330

    Perform operations with semaphore

--- Semaphore.cpp
+++ Semaphore.cpp
@@ -17,6 +17,8 @@ double *Lands;
 int DataSize = 0;
 double totalLands = 0;
 int threadcnt = 0;
+int readCount = 0;
+bool endthreads;
 
 struct GlobalResult
 {
@@ -96,6 +98,20 @@ int main()
             exit(1);
         }
     }
+
+    while (true)
+    {
+        WaitForSingleObject(printSem, INFINITE);
+        WaitForSingleObject(threads, INFINITE);
+        if (endthreads)
+        {
+            ReleaseSemaphore(threads, 1, NULL);
+            break;
+        }
+        ReleaseSemaphore(threads, 1, NULL);
+        ReleaseSemaphore(parent, 1, NULL);
+    }
+
     return 0;
 }
 
@@ -104,4 +120,100 @@ DWORD WINAPI F(LPVOID param)
     MyParam *input = static_cast<MyParam *>(param);
     DWORD threadID = input->threadID;
     GlobalResult *globalResult = input->globalResult;
+
+    auto start_time = high_resolution_clock::now();
+    double localBest = -1;
+
+    srand(static_cast<unsigned>(time(NULL)) + threadID);
+    double *distribution = new double[DataSize]();
+
+    while (true)
+    {
+        double sum[3] = {0, 0, 0};
+
+        for (int i = 0; i < DataSize; i++)
+        {
+            int randomindex = rand() % 3;
+            sum[randomindex] += Lands[i];
+            distribution[i] = randomindex;
+        }
+
+        double newdiff = abs(sum[0] - 0.4 * totalLands) + abs(sum[1] - 0.4 * totalLands) + abs(sum[2] - 0.2 * totalLands);
+        bool writer = false;
+
+        if (newdiff < localBest || localBest == -1)
+        {
+            WaitForSingleObject(q, INFINITE);
+            WaitForSingleObject(r, INFINITE);
+            ReleaseSemaphore(q, 1, NULL);
+            if (readCount == 0)
+            {
+                WaitForSingleObject(rw, INFINITE);
+            }
+            readCount++;
+
+            ReleaseSemaphore(r, 1, NULL);
+            localBest = globalResult->bestDiff;
+
+            if (newdiff < globalResult->bestDiff || globalResult->bestDiff == -1)
+            {
+                writer = true;
+            }
+
+            WaitForSingleObject(r, INFINITE);
+            readCount--;
+
+            if (readCount == 0)
+            {
+                ReleaseSemaphore(rw, 1, NULL);
+            }
+            ReleaseSemaphore(r, 1, NULL);
+        }
+
+        if (writer)
+        {
+            WaitForSingleObject(q, INFINITE);
+            WaitForSingleObject(rw, INFINITE);
+            ReleaseSemaphore(q, 1, NULL);
+            if (newdiff < globalResult->bestDiff || globalResult->bestDiff == -1)
+            {
+                for (int i = 0; i < DataSize; i++)
+                {
+                    globalResult->bestResult[i + 1] = distribution[i];
+                }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    globalResult->bestResult[DataSize + i + 1] = sum[i];
+                }
+
+                globalResult->bestThreadID = threadID;
+                globalResult->bestDiff = newdiff;
+                localBest = globalResult->bestDiff;
+
+                ReleaseSemaphore(printSem, 1, NULL);
+                WaitForSingleObject(parent, INFINITE);
+            }
+
+            ReleaseSemaphore(rw, 1, NULL);
+        }
+
+        auto end_time = high_resolution_clock::now();
+        auto elapsed_time = duration_cast<duration<double>>(end_time - start_time).count();
+
+        if (elapsed_time >= runtime)
+        {
+            break;
+        }
+    }
+
+    WaitForSingleObject(threads, INFINITE);
+
+    if (threadcnt == 1) {
+        endthreads = true;
+        ReleaseSemaphore(printSem, 1, NULL);
+    }
+
+    threadcnt--;
+    ReleaseSemaphore(threads, 1, NULL);
 }
