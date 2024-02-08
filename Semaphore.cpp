57b9a4107d9a8394422ccfbcc24511358f86d544
mmk <mmkkarbalaei@gmail.com>
2024-02-08 22:25:02 +0330

    read dataset file

--- Semaphore.cpp
+++ Semaphore.cpp
@@ -8,7 +8,18 @@
 using namespace std;
 using namespace std::chrono;
 
+#define Filepath "\\dataset\\"
 
-int main(){
+int main()
+{
+    string filename;
+    cout << "Enter dataset filename(1-6): ";
+    cin >> filename;
+    ifstream file(Filepath + filename + ".txt");
+    if (!file)
+    {
+        cout << "Cannot open file: " << filename << endl;
+        return 1;
+    }
     return 0;
 }
